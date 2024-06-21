d5ca3de2ef862f7b3c8cd2cfbb60d0a286012c60
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2024-06-21 12:39:45 +0200

    data structure parts

---
+++ Student.cs
@@ -0,0 +1,9 @@
+﻿public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double GPA { get; set; }
+    public string Major { get; set; }
+    public string University { get; set; }
+    public object ID { get; internal set; }
+}
